dd4f0a88fa04b0588d24f51828b56a4be5b782a3
jumpdev123 <jump.jjnn@gmail.com>
2022-08-18 13:15:53 +0700

    ep3_firt

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layouts/Layout'
+import React from 'react'
+
+type Props = {}
+
+export default function index({}: Props) {
+  return (
+    <Layout><div>jump</div></Layout>
+  )
+}
