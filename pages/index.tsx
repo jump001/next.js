58e238b8cc3329c9f4056fe8ac24c3a1b665c845
jumpdev123 <jump.jjnn@gmail.com>
2022-08-22 19:30:49 +0700

    end_partLearn-redux-toolkit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Layout from '@/components/Layouts/Layout'
+import { userSelector } from '@/store/slices/userSlices'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 type Props = {}
 
 export default function index({}: Props) {
+  const user = useSelector(userSelector)
   return (
-    <Layout><div>jump</div></Layout>
+    <Layout><div>jump{user.username}</div></Layout>
   )
 }
---
+++ store/slices/userSlices.ts
@@ -0,0 +1,35 @@
+import { UserData } from "@/models/user.model";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "@/store/store";
+
+
+interface UserState {
+    username: string;
+    accessToken: string;
+    error?: string;
+    isAuthenticated: boolean;
+    isAuthenticating: boolean;
+    user?: UserData;
+  }
+
+
+  const initialState: UserState = {
+    username: "jirasak keawdham",
+    accessToken: "",
+    isAuthenticated: false,
+    isAuthenticating: true,
+    user: undefined,
+  };
+
+
+  const userSlice = createSlice({
+    name: "user",
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (builder) => {},
+  });
+
+// export common user selector
+  export const userSelector = (store: RootState) => store.user;
+  // // export reducer
+export default userSlice.reducer;
